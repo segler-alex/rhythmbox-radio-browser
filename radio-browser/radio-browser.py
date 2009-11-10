315f51db82da6d3608b09ada166731f036b25264
segler-alex <segler_alex@web.de>
2009-11-10 14:42:48 +0100

    changed some licensing information

--- radio-browser/radio-browser.py
+++ radio-browser/radio-browser.py
@@ -1,19 +1,19 @@
-#    This file is part of IcecastPlugin for Rhythmbox.
+#    This file is part of Radio-Browser-Plugin for Rhythmbox.
 #
 #    Copyright (C) 2009 <segler_alex@web.de>
 #
-#    IcecastPlugin is free software: you can redistribute it and/or modify
+#    Radio-Browser-Plugin is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    IcecastPlugin is distributed in the hope that it will be useful,
+#    Radio-Browser-Plugin is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with IcecastPlugin.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Radio-Browser-Plugin.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import rb
